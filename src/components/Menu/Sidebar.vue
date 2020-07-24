d287167f0f945b89d51a2aa1333a8ad0dd3551d7
Michael Phelan <mphelan@g.harvard.edu>
2020-07-24 15:10:40 -0400

    artifacts for vue-sidebar-menu tutorial https://regenrek.com/posts/how-to-create-an-animated-vue-sidebar-menu-with-vue-observable/

---
+++ src/components/Menu/Sidebar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="sidebar">
+    <div class="sidebar-backdrop" @click="closeSidebarPanel" v-if="isPanelOpen"></div>
+    <transition name="slide"><div v-if="isPanelOpen" class="sidebar-panel"><slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+<script>
+  export default {
+  data: () => ({
+  isPanelOpen: true
+  }),
+  methods: {
+  closeSidebarPanel() {
+  this.isPanelOpen = false
+  }
+  }
+  }
+</script>
+<style>
+    .slide-enter-active,
+    .slide-leave-active
+    {
+        transition: transform 0.2s ease;
+    }
+
+    .slide-enter,
+    .slide-leave-to {
+        transform: translateX(-100%);
+        transition: all 150ms ease-in 0s
+    }
+
+    .sidebar-backdrop {
+        background-color: rgba(0,0,0,.5);
+        width: 100vw;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+        cursor: pointer;
+    }
+
+    .sidebar-panel {
+        overflow-y: auto;
+        background-color: #130f40;
+        position: fixed;
+        left: 0;
+        top: 0;
+        height: 100vh;
+        z-index: 999;
+        padding: 3rem 20px 2rem 20px;
+        width: 300px;
+    }
+</style>
