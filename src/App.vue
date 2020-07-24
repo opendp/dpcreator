083fb47a55dc7a3dc43a7aebeffb07d4c0c3dc14
Michael Phelan <mphelan@g.harvard.edu>
2020-07-24 15:07:27 -0400

    replaced boilerplate example with boilerplate vue-sidebar-menu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <example msg="Welcome to Example"/>
+    <nav class="main-nav">
+      <div class="logo">my.company</div>
+      <Burger></Burger>
+    </nav>
+    <Sidebar><ul class="sidebar-panel-nav">
+	<li><a href="#home">Home</a></li>
+	<li><a href="#about">About</a></li>
+	<li><a href="#contact">Contact</a></li>
+      </ul>
+    </Sidebar>
   </div>
 </template>
-
 <script>
-import example from './components/example.vue'
+  import Burger from './components/Menu/Burger.vue';
+  import Sidebar from './components/Menu/Sidebar.vue';
 
-export default {
-  name: 'App',
+  export default {
+  name: 'app',
   components: {
-   example
+  Burger, Sidebar
   }
-}
+  }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+html {
+   height: 100%;
+   overflow:hidden;
+ }
+
+ body {
+   border: 0; margin: 0; padding: 0;
+   font-family: 'Lato';
+   height: 100%;
+   background: rgb(101,31,87);
+   background: linear-gradient(45deg, rgba(101,31,87,1) 0%, rgba(225,113,87,1) 48%, rgba(249,248,113,1) 100%);
+ }
+
+ .logo {
+   align-self: center;
+   color: #fff;
+   font-weight: bold;
+   font-family: 'Lato'
+ }
+
+ .main-nav {
+   display: flex;
+   justify-content: space-between;
+   padding: 0.5rem 0.8rem;
+ }
+
+ ul.sidebar-panel-nav {
+   list-style-type: none;
+ }
+
+ ul.sidebar-panel-nav > li > a {
+   color: #fff;
+   text-decoration: none;
+   font-size: 1.5rem;
+   display: block;
+   padding-bottom: 0.5em;
+ }
 </style>
