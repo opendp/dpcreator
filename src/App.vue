9fed96048b3d5939b5dbb8602964f420117a9678
Michael Phelan <mphelan@g.harvard.edu>
2020-08-04 16:55:52 -0400

    reformatting, modifications for content pane inclusion, styles

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,55 @@
 <template>
 <div id="app">
-  <Sidebar>
-    <ul class="sidebar-panel-nav">
-      <li>Introduction</li>
-      <ul class="sidebar-panel-subnav">
-        <li><a href="#background">Background</a></li>
+  <div id="navigation">
+    <Sidebar>
+      <ul class="sidebar-panel-nav">
+	<li>Introduction</li>
+	<ul class="sidebar-panel-subnav">
+          <li><a href="#background">Background</a></li>
+	</ul>
+	<li>Import Data</li>
+	<ul class="sidebar-panel-subnav">
+          <li><a href="#upload-dataset">Upload dataset</a></li>
+          <li><a href="#confirm-dataset">Confirm dataset</a></li>
+          <li><a href="#select-dataset-type">Select dataset type</a></li>
+          <li><a href="#confirm-dataset-contents">Confirm contents of dataset</a></li>
+          <li><a href="#specify-dataset-info">Specify dataset information</a></li>                </ul>
+	<li>Specify Metadata</li>
+	<ul class="sidebar-panel-subnav">
+          <li><a href="#create-custom-vars">Create custom variables</a></li>
+          <li><a href="#confirm-var-types">Confirm variable types</a></li>
+          <li><a href="#set-var-ranges">Set variable ranges</a></li>
+          <li><a href="#confirm-vars-of-interest">Confirm variables of interest</a></li>
+	</ul>
+	<li>Set Parameters</li>
+	<ul class="sidebar-panel-subnav">
+          <li><a href="#set-priv-loss-params">Set privacy loss parameters</a></li>
+	</ul>
+	<li>Create Statistics</li>
+	<ul class="sidebar-panel-subnav">
+          <li><a href="#create-stats">Create statistics</a></li>
+	</ul>
+	<li>Generate Report</li>
+	<ul class="sidebar-panel-subnav">
+          <li><a href="#confirm-err-and-submit">Confirm error and submit</a></li>
+          <li><a href="#view-report">View report</a></li>
+	</ul>
       </ul>
-      <li>Import Data</li>
-      <ul class="sidebar-panel-subnav">
-        <li><a href="#upload-dataset">Upload dataset</a></li>
-        <li><a href="#confirm-dataset">Confirm dataset</a></li>
-        <li><a href="#select-dataset-type">Select dataset type</a></li>
-        <li><a href="#confirm-dataset-contents">Confirm contents of dataset</a></li>
-        <li><a href="#specify-dataset-info">Specify dataset information</a></li>                </ul>
-      <li>Specify Metadata</li>
-      <ul class="sidebar-panel-subnav">
-        <li><a href="#create-custom-vars">Create custom variables</a></li>
-        <li><a href="#confirm-var-types">Confirm variable types</a></li>
-        <li><a href="#set-var-ranges">Set variable ranges</a></li>
-        <li><a href="#confirm-vars-of-interest">Confirm variables of interest</a></li>
-      </ul>
-      <li>Set Parameters</li>
-      <ul class="sidebar-panel-subnav">
-        <li><a href="#set-priv-loss-params">Set privacy loss parameters</a></li>
-      </ul>
-      <li>Create Statistics</li>
-      <ul class="sidebar-panel-subnav">
-        <li><a href="#create-stats">Create statistics</a></li>
-      </ul>
-      <li>Generate Report</li>
-      <ul class="sidebar-panel-subnav">
-        <li><a href="#confirm-err-and-submit">Confirm error and submit</a></li>
-        <li><a href="#view-report">View report</a></li>
-      </ul>
-    </ul>
-  </Sidebar>
+    </Sidebar>
+  </div>
+  <div id="canvas">
+    <Content></Content>
+  </div>
 </div>
 </template>
 <script>
 import Sidebar from './components/Menu/Sidebar.vue';
-
+import Content from './components/Content.vue';
 export default {
     name: 'app',
     components: {
-	Sidebar
+      Sidebar,
+      Content
     },
 }
 </script>
@@ -51,41 +57,53 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 html {
-   height: 100%;
-   overflow:hidden;
- }
-
- body {
-   border: 0; margin: 0; padding: 0;
-   font-family: 'Lato';
-   height: 100%;
-   background: rgba(255,255,255,0.5);
- }
-
- ul.sidebar-panel-nav {
-   list-style-type: none;
- }
-
- ul.sidebar-panel-nav > li {
-   color: rgba(8,8,8,0.5);
-   text-decoration: none;
-   font-family: 'Roboto', sans-serif;
-   font-size: 1.5rem;
-   font-weight: bold;
-   Display: block;
-   padding-bottom: 0.5em;
+    height: 100%;
+    overflow:hidden;
 }
 
- ul.sidebar-panel-subnav {
-   list-style-type: none;
- }
+body {
+    border: 0; margin: 0; padding: 0;
+    color: black;
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.5rem;
+    font-weight: bold;
+    Display: block;
+    padding-bottom: 0.5em;
+    height: 100%;
+    background: rgba(255,255,255,0.5);
+    float: right;
+    width: 60%
+}
 
- ul.sidebar-panel-subnav > li > a {
-   color: rgba(86,86,86,0.5);
-   text-decoration: none;
-   font-family: 'Roboto', sans-serif;
-   font-size: 1.5rem;
-   display: block;
-   padding-bottom: 0.5em;
- }
+ul.sidebar-panel-nav {
+    list-style-type: none;
+}
+
+ul.sidebar-panel-nav > li {
+    color: rgba(8,8,8,0.5);
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.5rem;
+    font-weight: bold;
+    Display: block;
+    padding-bottom: 0.5em;
+}
+
+ul.sidebar-panel-subnav {
+    list-style-type: none;
+}
+
+ul.sidebar-panel-subnav > li > a {
+    color: rgba(86,86,86,0.5);
+    text-decoration: none;
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.5rem;
+    display: block;
+    padding-bottom: 0.5em;
+}
+button, input[type=button] {
+    background: blue;
+    color: white;
+}
 </style>
