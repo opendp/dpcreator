d0ea4c5726441dac8365931c9f8ed213adc9c60d
Michael Phelan <mphelan@g.harvard.edu>
2020-09-30 12:12:20 -0400

    removal of files associated with Vuetify based site

--- src/components/Content.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <p>Welcome!</p>
-    <p>Welcome to OpenDP, a tool that helps data depositors release privacy preserving statistics (such as means or histograms) by distributing a "privacy budget" across different possible calculations.</p>
-     <button>I UNDERSTAND. LET'S GET STARTED!</button>
-  </div>
-</template>
