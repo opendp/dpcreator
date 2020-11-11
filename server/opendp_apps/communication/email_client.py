e33b0552f0120fc037e6f704ff72e578772b5c4f
ecowan <ethan24@gmail.com>
2020-11-10 19:20:40 -0500

    Email client, SendGrid, etc.

---
+++ server/opendp_apps/communication/email_client.py
@@ -0,0 +1,50 @@
+import os
+
+from sendgrid import sendgrid, Email, Content, Mail, To
+
+
+class SendGridAPIError(Exception):
+    pass
+
+
+class EmailClient(object):
+
+    # TODO: Make this a product-wide address.
+    # To verify a new account:
+    #   1. Go to https://app.sendgrid.com/settings/sender_auth/senders
+    #   2. Click "verify new sender" and proceed
+    default_from_email = 'ecowan@g.harvard.edu'
+
+    def __init__(self, from_email=None, api_key=None):
+        self.from_email = from_email if from_email else self.default_from_email
+        if not api_key:
+            try:
+                self.api_key = os.environ.get('SENDGRID_API_KEY')
+            except KeyError:
+                raise SendGridAPIError("SENDGRID_API_KEY must be passed as an argument or"
+                                       " set as an environment variable")
+        else:
+            self.api_key = api_key
+        self.sendgrid_client = sendgrid.SendGridAPIClient(self.api_key)
+
+    def send(self, to_email=None, subject=None, content=None, content_type=None):
+        from_email = Email(self.from_email)
+        to_email = To(to_email)
+        content = Content(content_type, content)
+        mail = Mail(from_email, to_email, subject, content)
+        return self.sendgrid_client.client.mail.send.post(request_body=mail.get())
+
+
+if __name__ == '__main__':
+    apikey_message = "Current API Key: " + os.environ.get('SENDGRID_API_KEY')
+    print("-"*(len(apikey_message)+1))
+
+    print("Current API Key: ", os.environ.get('SENDGRID_API_KEY'))
+    c = EmailClient()
+    print("From Email: ", c.from_email)
+    print("-"*(len(apikey_message)+1))
+    result = c.send(to_email='ecowan@g.harvard.edu', subject='test test',
+                    content='hi', content_type='text/plain')
+    print("Message Sent")
+    print("Status Code: ", result.status_code)
+    print("-"*(len(apikey_message)+1))
---
+++ server/opendp_apps/communication/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.db.models import CASCADE
+
+
+class EmailMessage(models.Model):
+    class ContentTypeChoices(models.TextChoices):
+        PLAIN_TEXT = 'text/plain'
+        HTML = 'application/html'
+    content_type = models.CharField(choices=ContentTypeChoices.choices)
+    message_template = models.TextField()
+
+
+class Email(models.Model):
+    from_email = models.CharField(max_length=128)
+    to_email = models.CharField(max_length=128)
+    subject = models.CharField(max_length=128)
+    message = models.ForeignKey(EmailMessage, on_delete=CASCADE)
