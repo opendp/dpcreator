07d30e80a2f049d24e878c17678d098b79e03224
ellenk <ekraffmiller@hmdc.harvard.edu>
2020-12-03 11:34:38 -0500

    adding Vue Login page

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/About">About</router-link> |
+      <router-link to="/login">Login</router-link>
+
     </div>
     <router-view/>
   </div>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Login from '../views/Login.vue'
 Vue.use(VueRouter)
 
   const routes = [
@@ -19,7 +19,13 @@ Vue.use(VueRouter)
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
-  }
+  },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
+
 ]
 
 const router = new VueRouter({
