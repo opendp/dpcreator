56b1389d20619ec4c1165a2151c1c2f0f031bbe7
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-01-05 10:09:33 -0500

    added logic to display if user is authenticated

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="home-view">
     <h1>Home</h1>
-  </div>
+      <h3 v-if="isAuthenticated">authenticated!</h3>
+
+    </div>
 </template>
 
 <script>
+
+import {
+  mapActions,
+  mapGetters,
+  mapState,
+} from 'vuex';
+
 export default {
   name: 'home',
-};
+
+  computed: {
+    ...mapGetters('auth', ['isAuthenticated']),
+  },
+}
+
 </script>
