8eba6736bca3e080bb42134984fc91104f6efbbc
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-01-05 10:08:45 -0500

    commit Google signin Vue component

--- client/src/main.js
+++ client/src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import  axios from  'axios'
+import GSignInButton from 'vue-google-signin-button'
+
 Vue.config.productionTip = false
+Vue.use(GSignInButton)
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = 'csrftoken';
 
 new Vue({
   router,
