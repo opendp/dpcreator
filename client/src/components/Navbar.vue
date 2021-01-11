3a287eac3035b8d2012775f0dde8351db4f6583d
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-01-11 15:29:48 -0500

    add new Vue components and views

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul class="navbar" id="navbar">
+    <li><router-link to="/logout">logout</router-link></li>
+    <li><router-link to="/about">about</router-link></li>
+    <li><router-link to="/password_reset">reset password</router-link></li>
+  </ul>
+</template>
+
+<script>
+
+import auth from '../api/auth'
+import {
+  mapActions,
+  mapGetters,
+  mapState,
+} from 'vuex';
+
+export default {
+  name: 'navbar',
+  created() {
+    this.$store.dispatch('auth/fetchUser')
+  },
+  computed: {
+    ...mapGetters('auth', ['isAuthenticated']),
+    ...mapState('auth', ['user']),
+  },
+}
+</script>
+
+
+<style scoped>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+
+li {
+  display: inline;
+  float: right;
+}
+
+a {
+  color: #1a1a1a;
+  display: inline-block;
+  text-align: center;
+  padding: 6px 12px;
+  text-decoration: none;
+  font-size: 14px;
+}
+
+a:hover {
+  color: #cc0000;
+  text-decoration: none;
+}
+</style>
