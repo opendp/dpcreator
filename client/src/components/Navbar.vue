0bfe3a545d28860e707a4bfced6bf9cd07be62d1
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-01-14 15:13:52 -0500

    Vuetify version of Navbar, commented out for now

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,5 +1,24 @@
 <template>
-  <ul class="navbar" id="navbar">
+  <!--
+   <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+     <v-btn :key="Home-header-link" text rounded @click="$router.push('home')" >Home</v-btn>
+     <template v-if="isAuthenticated">
+      <v-btn :key="Logout-header-link" text rounded @click="$router.push('logout')" >Logout</v-btn>
+       <v-btn :key="PasswordReset-header-link" text rounded @click="$router.push('password_reset')" >Reset Password</v-btn>
+     </template>
+     <template v-if="!isAuthenticated">
+       <v-btn :key="Login-header-link" text rounded @click="$router.push('login')" >Login</v-btn>
+     </template>
+
+      v-btn @click="toggleTheme" text rounded>
+        Toggle Theme
+      </v-btn
+
+    </v-app-bar>
+    -->
+<ul class="navbar" id="navbar">
     <li><router-link to="/logout">logout</router-link></li>
     <li><router-link to="/about">about</router-link></li>
     <li><router-link to="/password_reset">reset password</router-link></li>
