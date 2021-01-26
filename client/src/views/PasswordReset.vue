10a0d3eefd44198554ec49e2a0078d2c968f1f57
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-01-26 15:59:25 -0500

    converted to Vuetify

--- client/src/views/PasswordReset.vue
+++ client/src/views/PasswordReset.vue
@@ -1,30 +1,38 @@
 <template>
-  <div id="password-reset-view">
-    <h1>Reset Password</h1>
-    <template v-if="emailLoading">
-      loading...
-    </template>
-    <template v-else-if="!emailCompleted">
-      <form @submit.prevent="submit">
-        <input v-model="inputs.email" type="text" id="email" placeholder="email">
-      </form>
-      <button @click="sendPasswordResetEmail(inputs)">
-        send email
-      </button>
-      <span class="error" v-show="emailError">
-        A error occured while processing your request.
-      </span>
-    </template>
-    <template v-else>
+
+
+   <v-card width="400" class="mx-auto mt-5">
+    <v-card-title>
+      <h1 class="display-1">Reset Password</h1>
+    </v-card-title>
+    <v-card-text>
+     <template v-if="!emailCompleted">
+      <v-form @submit.prevent="submit">
+        <v-text-field v-model="inputs.email" id="email" placeholder="email"></v-text-field>
+      </v-form>
+      </template>
+       <template v-else>
       <div>
         Check your inbox for a link to reset your password. If an email doesn't appear within a few
         minutes, check your spam folder.
       </div>
-      <router-link to="/login">
+      </template>
+      <v-divider></v-divider>
+      <v-card-actions>
+
+     <template v-if="!emailCompleted">
+          <v-btn @click="sendPasswordResetEmail(inputs)">
+        send email
+      </v-btn>
+      </template>
+      <template v-else>
+       <router-link to="/login">
         return to login page
       </router-link>
-    </template>
-  </div>
+      </template>
+      </v-card-actions>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
