1b781342bfa91cb885d546bd45581f49de54d9b6
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-02-04 15:10:26 -0500

    add trailing slash to url, so that DataverseUserInfoView is invoked with a post request

--- server/opendp_apps/dataverses/urls.py
+++ server/opendp_apps/dataverses/urls.py
@@ -3,7 +3,7 @@ from opendp_apps.dataverses.views import DataverseUserInfoView
 # This may move to DRF..
 urlpatterns = [
 
-    path(f'dv-info/get-user-info',
+    path(f'dv-info/get-user-info/',
          DataverseUserInfoView.as_view(),
          name='view_get_dv_user_info'),
 ]
