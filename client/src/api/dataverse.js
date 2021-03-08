9a1324e8f4caafdb0310c1da601141aaa7050edb
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-03-08 14:23:14 -0500

    cleanup and add comments

--- client/src/api/dataverse.js
+++ client/src/api/dataverse.js
@@ -1,12 +1,15 @@
 import session from './session';
 
 export default {
-     getUserInfo(apiGeneralToken, siteUrl) {
-         const formData = new FormData();
-         formData.append("apiGeneralToken", apiGeneralToken);
-         formData.append("siteUrl", siteUrl);
-         return session.post('/api/dv-user/', formData);
-     },
+    /**
+     * For debugging only TODO: remove when new dataset apis are ready
+     * @param apiGeneralToken
+     * @param siteUrl
+     * @param datasetPid
+     * @param fileId
+     * @param filePid
+     * @returns {Promise<AxiosResponse<any>>}
+     */
     getDatasetInfo(apiGeneralToken, siteUrl, datasetPid, fileId, filePid) {
         const formData = new FormData();
         formData.append("apiGeneralToken", apiGeneralToken);
@@ -16,11 +19,32 @@ export default {
         formData.append("filePid", filePid);
         return session.post('/api/dv-dataset/', formData);
     },
-    // TODO:
-    //  this should call a Django endpoint that will get the
-    // user data from Dataverse, create a DataverseUser object in the db,
-    // and return the DataverseUser object
-    updateDataverseUser(OpenDPUserId, siteUrl, apiGeneralToken) {
+
+    /**
+     *  check if the OpenDPUser is associated with a DataverseUser,
+     *  and if so, get the apiToken and siteUrl from the DataverseUser
+     *  to call dv-user, and update the DataverseUser with the latest
+     *  user info from dataverse.
+     *  TODO: update with new API call when ready
+     * @param OpenDPUserId
+     * @returns {Promise<AxiosResponse<any>>} DataverseUser object
+     */
+    updateDataverseUser(OpenDPUserId) {
+        console.log('calling API updateDataverseUser ' + OpenDPUserId + ',' + siteUrl + ',' + apiGeneralToken)
+        const formData = new FormData();
+        formData.append("apiGeneralToken", apiGeneralToken);
+        formData.append("siteUrl", siteUrl);
+        return session.post('/api/dv-user/', formData);
+    },
+    /**
+     * Use token and siteUrl to get Dataverse user info, create DataverseUser object
+     * for the OpenDPUser
+     * @param OpenDPUserId
+     * @param siteUrl
+     * @param apiGeneralToken
+     * @returns {Promise<AxiosResponse<any>>}  DataverseUser object
+     */
+    createDataverseUser(OpenDPUserId, siteUrl, apiGeneralToken) {
         console.log('calling API updateDataverseUser ' + OpenDPUserId + ',' + siteUrl + ',' + apiGeneralToken)
         const formData = new FormData();
         formData.append("apiGeneralToken", apiGeneralToken);
