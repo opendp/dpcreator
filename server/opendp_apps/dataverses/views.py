3627f246dd9ea1fcbacbe6ea4b6022e0b8092dfd
ecowan <ethan24@gmail.com>
2021-03-11 13:38:12 -0500

    Starting on new DataverseUser endpoint

--- server/opendp_apps/dataverses/views.py
+++ server/opendp_apps/dataverses/views.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 from django.contrib.auth.decorators import user_passes_test
 from django.http import JsonResponse
+from rest_framework.views import APIView
 
 from opendp_apps.utils.view_helper import get_json_error, get_json_success
 from opendp_apps.dataverses.models import ManifestTestParams
@@ -17,4 +18,12 @@ def view_as_dict(request, object_id):
 
     return JsonResponse(get_json_success('Success', data=mparams.as_dict()))
 
+
+class DataverseUserView(APIView):
+
+    def post(self, request, *args, **kwargs):
+        pass
+
+    def get(self, request, *args, **kwargs):
+        pass
 
