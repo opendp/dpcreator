1262b249a64096225f87770bc488dc59c772b1e9
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-03-22 16:05:50 -0400

    add vue-home url name

--- server/opendp_project/urls.py
+++ server/opendp_project/urls.py
@@ -62,15 +62,15 @@ urlpatterns = [
     path('api/dv-user/', DataverseUserView.as_view(), name='dv-user'),
     # For testing
     path('dv-mock-api/', include('opendp_apps.dataverses.mock_urls')),
-    url(r'^user-details/$',
-        TemplateView.as_view(template_name="user_details.html"),
-        name='user-details'),
-    url(r'^rest-auth/', include('dj_rest_auth.urls')),
-    url(r'^rest-auth/registration/', OpenDPRegister.as_view(), name='opendp-register'),
-    url(r'^rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    url(r'^account/', include('allauth.urls')),
-    url(r'^accounts/profile/$', RedirectView.as_view(url='/', permanent=True), name='profile-redirect'),
-    url(r'^rest-auth/google/$', GoogleLogin.as_view(), name='google-login'),
-    # Putting all vue-related views under "ui/" for now to separate from the api.
-    url(r'^.*$', TemplateView.as_view(template_name="index.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, kwargs={'show_indexes': True})
+                  url(r'^user-details/$',
+                      TemplateView.as_view(template_name="user_details.html"),
+                      name='user-details'),
+                  url(r'^rest-auth/', include('dj_rest_auth.urls')),
+                  url(r'^rest-auth/registration/', OpenDPRegister.as_view(), name='opendp-register'),
+                  url(r'^rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+                  url(r'^account/', include('allauth.urls')),
+                  url(r'^accounts/profile/$', RedirectView.as_view(url='/', permanent=True), name='profile-redirect'),
+                  url(r'^rest-auth/google/$', GoogleLogin.as_view(), name='google-login'),
+                  # Putting all vue-related views under "ui/" for now to separate from the api.
+                  url(r'^.*$', TemplateView.as_view(template_name="index.html"), name='vue-home'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, kwargs={'show_indexes': True})
