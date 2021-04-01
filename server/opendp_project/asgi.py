1a82f319212250c45fe258baeb3db8e753d5d0c7
Raman Prasad <raman_prasad@harvard.edu>
2021-04-01 15:44:18 -0400

    remove un-needed line

--- server/opendp_project/asgi.py
+++ server/opendp_project/asgi.py
@@ -12,7 +12,6 @@ import os
 import django
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opendp_project.settings')
 
