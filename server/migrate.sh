b9494c5f34ea244783ad0ebdc4c39fdd32bd6c74
Raman Prasad <raman_prasad@harvard.edu>
2021-04-07 14:07:25 -0400

    fixture update for #134

--- server/migrate.sh
+++ server/migrate.sh
@@ -34,7 +34,6 @@ echo "| (for dev)           |"
 echo "-----------------------"
 python /code/server/manage.py loaddata opendp_apps/dataverses/fixtures/test_dataverses_01.json \
   opendp_apps/dataverses/fixtures/test_manifest_params_04.json \
-  opendp_apps/dataverses/fixtures/test_dataverse_handoff_01.json \
   opendp_apps/dataverses/fixtures/test_opendp_users_01.json \
   opendp_apps/dataverses/fixtures/admin_emailaddress.json
 exec "$@"
