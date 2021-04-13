d611a72a0d708dc37dbe997d9f8ddd450d0481ae
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-04-13 14:26:47 -0400

    implement basic login

--- client/src/main.js
+++ client/src/main.js
@@ -1,16 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from './store'
+import axios from 'axios'
+import GSignInButton from 'vue-google-signin-button'
 import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
-
+Vue.use(GSignInButton)
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.xsrfCookieName = 'csrftoken';
 import VueWaypoint from "vue-waypoint";
 
 Vue.use(VueWaypoint);
 
 new Vue({
   router,
+  store,
   vuetify,
   render: h => h(App)
 }).$mount("#app");
--- client/src/views/LogIn.vue
+++ client/src/views/LogIn.vue
@@ -6,14 +6,13 @@
           <h1 class="title-size-1">Log in</h1>
           <v-form v-model="validLoginForm">
             <v-text-field
-                v-model="email"
-                label="Email"
+                v-model="inputs.username"
+                label="Username"
                 required
-                :rules="emailRules"
-                type="email"
+                type="text"
             ></v-text-field>
             <v-text-field
-                v-model="password"
+                v-model="inputs.password"
                 :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
                 :type="showPassword ? 'text' : 'password'"
                 name="input-10-1"
@@ -30,7 +29,7 @@
                   class="mr-3"
                   color="primary"
                   :disabled="!validLoginForm"
-                  @click="handleLogin"
+                  @click="login(inputs)"
               >Log in
               </v-btn
               >
@@ -39,6 +38,12 @@
               </v-btn
               >
             </div>
+            <v-alert dense outlined type="error" v-show="error">
+              An error occurred while processing your request.
+              <ul>
+                <li v-for="err in loginErrors">{{ err }}</li>
+              </ul>
+            </v-alert>
           </v-form>
           <div class="my-5">
             <span
@@ -78,12 +83,43 @@
 <script>
 import SocialLoginButton from "../components/Accounts/SocialLoginButton.vue";
 import SocialLoginSeparator from "../components/Accounts/SocialLoginSeparator.vue";
+import {mapState} from 'vuex';
 
 export default {
-  name: "MyData",
+  name: "Login",
   components: {SocialLoginButton, SocialLoginSeparator},
+  computed: {
+    ...mapState('auth', ['error', 'user']),
+    ...mapState('dataverse', ['handoffId']),
+    loginErrors() {
+      let errs = [];
+      if (this.errorMessage != null) {
+        if (this.errorMessage['non_field_errors'] != null)
+          errs = errs.concat(this.errorMessage['non_field_errors']);
+      }
+      return errs;
+    },
+    error() {
+      return this.errorMessage != null;
+    }
+  },
   methods: {
-    handleLogin: function () {
+    login: function ({username, password}) {
+      this.errorMessage = null;
+      this.$store.dispatch('auth/login', {username, password})
+          .catch((data) => {
+            this.errorMessage = data;
+          })
+          .then(() => {
+            if (!this.error) {
+              this.checkDataverseUser();
+              this.$router.push('/welcome');
+            }
+          })
+          .catch((data) => {
+            console.log(data);
+            this.errorMessage = data
+          });
       localStorage.setItem("loggedUser", true);
       this.$router.push("/welcome");
     },
@@ -92,13 +128,29 @@ export default {
     },
     loginGithub: function () {
       alert("login with Github!");
+    },
+    checkDataverseUser() {
+      if (this.handoffId) {
+        this.$store.dispatch('auth/fetchUser')
+            .then(() => {
+              this.$store.dispatch('dataverse/updateDataverseUser', this.user['object_id'], this.handoffId)
+                  .catch(({data}) => console.log("error: " + data)).then(({data}) => console.log(data))
+            })
+      }
     }
+
   },
   data: () => ({
+    errorMessage: null,
+    googleSignInParams: {
+      client_id: '725082195083-1srivl3ra9mpc1q5ogi7aur17vkjuabg.apps.googleusercontent.com',
+    },
     validLoginForm: false,
     showPassword: false,
-    email: "",
-    password: "",
+    inputs: {
+      username: '',
+      password: '',
+    },
     emailRules: [
       v => !!v || "E-mail is required",
       v => /.+@.+\..+/.test(v) || "E-mail must be valid"
