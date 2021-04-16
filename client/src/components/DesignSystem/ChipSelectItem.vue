aaaf51fb4382a6998c369f4f0cf3c1eb6a1c9776
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-04-16 10:00:47 -0400

    add new files from agile monkees design

---
+++ client/src/components/DesignSystem/ChipSelectItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-chip
+      :v-bind="v_bind"
+      :input-value="input_value"
+      close
+      color="blue darken-2"
+      text-color="white"
+      @click="click"
+      @click:close="click_close"
+  ><strong>{{ item }}</strong>
+  </v-chip>
+</template>
+
+<script>
+export default {
+  name: "ChipSelectItem",
+  props: ["v_bind", "input_value", "click", "click_close", "item"]
+};
+</script>
---
+++ client/src/views/NotFoundPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="not-found-page d-flex flex-column align-center mt-10">
+    <p class="subtitle">404 Error</p>
+    <h2 class="title-size-1 mb-0">Sorry, this page isn't available.</h2>
+    <p class="my-10">
+      <router-link
+          :to="NETWORK_CONSTANTS.CONTACT_US.PATH"
+          class="text-decoration-none primary--text"
+      >
+        Get in touch
+      </router-link>
+      with the site owners for more information.
+    </p>
+    <Button
+        color="primary"
+        :click="() => $router.push(NETWORK_CONSTANTS.HOME.PATH)"
+        label="Go to Home"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.subtitle {
+  font-size: 25px;
+  font-weight: lighter;
+}
+</style>
+
+<script>
+import Button from "../components/DesignSystem/Button.vue";
+import NETWORK_CONSTANTS from "../router/NETWORK_CONSTANTS";
+
+export default {
+  components: {Button},
+  name: "NotFoundPage",
+  data: () => ({
+    NETWORK_CONSTANTS
+  })
+};
+</script>
