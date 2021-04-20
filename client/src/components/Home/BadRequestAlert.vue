0577462751cec62547af0a0c8dcd67347e5d14c8
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-04-20 10:54:21 -0400

    Add a component to Home page for reporting errors (missing handoff, bad handoff)

---
+++ client/src/components/Home/BadRequestAlert.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-container
+      :class="{
+      'mb-5': $vuetify.breakpoint.xsOnly,
+      'mb-16': $vuetify.breakpoint.smAndUp
+    }"
+  >
+    <v-row>
+      <v-col
+          offset-sm="1"
+          offset-md="0"
+          sm="10"
+          class="text-center mx-auto px-5"
+          md="7"
+      >
+        <v-alert
+            border="right"
+            colored-border
+            type="error"
+            elevation="2"
+            v-show=showAlert>
+          {{ alertText }}
+        </v-alert>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style lang="scss">
+.event-success-alert {
+  &.alert-wrapper {
+    position: relative;
+  }
+
+  .alert {
+    position: absolute !important;
+    top: 0;
+    right: 0;
+    z-index: 1;
+    @media (max-width: 600px) {
+      text-align: center;
+      width: 90%;
+      margin: 0 auto;
+      right: 5%;
+    }
+  }
+
+  .theme--light.v-sheet {
+    color: black;
+  }
+}
+</style>
+
+<script>
+export default {
+  name: "BadRequestAlert",
+  props: ["alertText", "showAlert"],
+};
+</script>
