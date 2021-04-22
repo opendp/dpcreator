d1f80f54a9a59db9d913c000c8c8830641591af1
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-04-22 12:50:56 -0400

    add django server to test environment, test Home page

--- client/cypress/integration/ui-spec.js
+++ client/cypress/integration/ui-spec.js
@@ -1,6 +1,22 @@
-describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true)
-  })
-})
+{
+  describe('My First Test', () => {
+    it('Does not do much!', () => {
+      expect(true).to.equal(true)
+    })
+  }),
+      describe('The Home Page', () => {
+        it('successfully loads', () => {
+          cy.visit('http://localhost:8000') // change URL to match your dev URL
+        })
+      }),
+      describe('The Home Page Title', () => {
+        it('successfully loads', () => {
+          cy.visit('http://localhost:8000') // change URL to match your dev URL
+          cy.get('h2') // 9.
+              .should('contain', 'Open')
+        })
+      })
+
+
+}
 
