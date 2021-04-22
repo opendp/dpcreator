fc278cd8612eac4fb30086d1b71cf21131fccbc8
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-04-22 13:23:04 -0400

    change cypress test url

--- client/cypress/integration/ui-spec.js
+++ client/cypress/integration/ui-spec.js
@@ -6,14 +6,14 @@
   }),
       describe('The Home Page', () => {
         it('successfully loads', () => {
-          cy.visit('http://localhost:8000') // change URL to match your dev URL
+            cy.visit('0.0.0.0:8000') // change URL to match your dev URL
         })
       }),
       describe('The Home Page Title', () => {
         it('successfully loads', () => {
-          cy.visit('http://localhost:8000') // change URL to match your dev URL
-          cy.get('h2') // 9.
-              .should('contain', 'Open')
+            cy.visit('0.0.0.0:8000') // change URL to match your dev URL
+            cy.get('h2') // 9.
+                .should('contain', 'Open')
         })
       })
 
