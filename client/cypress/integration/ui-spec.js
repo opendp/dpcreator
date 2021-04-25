e37b592d00a6009a61c130f0c0f994d04bd6f856
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-04-25 07:43:42 -0400

    test cypress error condition

--- client/cypress/integration/ui-spec.js
+++ client/cypress/integration/ui-spec.js
@@ -1,20 +1,26 @@
 {
   describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
-    })
+      it('Does not do much!', () => {
+          expect(true).to.equal(true)
+      })
   }),
       describe('The Home Page', () => {
-        it('successfully loads', () => {
-            cy.visit('/') // change URL to match your dev URL
-        })
+          it('successfully loads', () => {
+              cy.visit('/') // change URL to match your dev URL
+          })
       }),
+      //   describe('serve static', () => {
+      //     it('successfully loads', () => {
+      //         cy.visit('/code/server/static/dist/css/chunk-2b9c28d5.efbb04fc.css') // change URL to match your dev URL
+
+      //     })
+      //   })
       describe('The Home Page Title', () => {
-        it('successfully loads', () => {
-            cy.visit('/') // change URL to match your dev URL
-            cy.get('h2') // 9.
-                .should('contain', 'Open')
-        })
+          it('successfully loads', () => {
+              cy.visit('/') // change URL to match your dev URL
+              cy.get('thisdoesnotexist') // 9.
+                  .should('contain', 'Open')
+          })
       })
 
 
