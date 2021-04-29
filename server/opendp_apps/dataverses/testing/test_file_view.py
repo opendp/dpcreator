c7f93ea6586140459a2fc7398169ade313881ac1
ecowan <ethan24@gmail.com>
2021-04-29 17:07:23 -0400

    Switching create() and list() methods in dataverse_file_view.py

--- server/opendp_apps/dataverses/testing/test_file_view.py
+++ server/opendp_apps/dataverses/testing/test_file_view.py
@@ -9,13 +9,11 @@ from opendp_apps.dataverses.testing import schema_test_data
 from opendp_apps.model_helpers.msg_util import msgt
 
 
-
 class FileViewGetTest(TestCase):
 
     fixtures = ['test_dataverses_01.json',
                 'test_manifest_params_04.json',
                 'test_opendp_users_01.json']
-
 
     @requests_mock.Mocker()
     def test_10_successful_get(self, req_mocker):
@@ -39,16 +37,45 @@ class FileViewGetTest(TestCase):
                                    data={'handoff_id': handoff_obj.object_id,
                                          'user_id': '6c4986b1-e90d-48a2-98d5-3a37da1fd331'},
                                    content_type='application/json')
-        #print(response.json())
+        # print(response.json())
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json().get('success'), True)
-        self.assertTrue('dataset_schema_info' in response.json().get('data', {}))
-        self.assertTrue('file_schema_info' in response.json().get('data', {}))
+        self.assertEqual(response.json().get('data'), [])
 
     @requests_mock.Mocker()
-    def test_15_unsuccessful_get(self, req_mocker):
+    def test_12_successful_creation(self, req_mocker):
         """(15) Schema.org retrieved but file specific info is not found!"""
-        msgt(self.test_15_unsuccessful_get.__doc__)
+        msgt(self.test_12_successful_creation.__doc__)
+        # From fixture file: "test_manifest_params_04.json"
+        tparams = ManifestTestParams.objects.get(object_id='4bcad631-ce7c-475e-a569-29e71ee0b2ee')
+        handoff_req = tparams.make_test_handoff_object()
+        self.assertTrue(handoff_req.success is True)
+        handoff_obj = handoff_req.data
+
+        # The Mock url is for when the applications calls "Dataverse" to retrieve JSON-LD metadata
+        #
+        mock_url = ('http://127.0.0.1:8000/dv-mock-api/api/v1/datasets/export'
+                    '?exporter=schema.org&persistentId=doi:10.7910/DVN/PUXVDH'
+                    '&User-Agent=pydataverse&key=shoefly-dont-bother-m3')
+
+        schema_content = tparams.schema_org_content
+        req_mocker.get(mock_url, json=schema_content)
+
+        response = self.client.post('/api/dv-file/',
+                                    data={'handoff_id': handoff_obj.object_id,
+                                          'creator': '6c4986b1-e90d-48a2-98d5-3a37da1fd331'},
+                                    content_type='application/json')
+        # print(response.json())
+        # print('response.status_code', response.status_code)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json().get('success'), True)
+        # 'message' should only appear when there's an error and we are displaying the error message
+        self.assertTrue('message' not in response.json())
+
+    @requests_mock.Mocker()
+    def test_15_unsuccessful_creation(self, req_mocker):
+        """(15) Schema.org retrieved but file specific info is not found!"""
+        msgt(self.test_15_unsuccessful_creation.__doc__)
 
         # From fixture file: "test_manifest_params_04.json"
         tparams = ManifestTestParams.objects.get(object_id='4bcad631-ce7c-475e-a569-29e71ee0b2ee')
@@ -68,16 +95,15 @@ class FileViewGetTest(TestCase):
 
         req_mocker.get(mock_url, json=schema_content)
 
-        response = self.client.get('/api/dv-file/',
-                                   data={'handoff_id': handoff_obj.object_id,
-                                         'user_id': '6c4986b1-e90d-48a2-98d5-3a37da1fd331'},
-                                   content_type='application/json')
-        #print(response.json())
-        print('response.status_code', response.status_code)
+        response = self.client.post('/api/dv-file/',
+                                    data={'handoff_id': handoff_obj.object_id,
+                                          'user_id': '6c4986b1-e90d-48a2-98d5-3a37da1fd331'},
+                                    content_type='application/json')
+        # print(response.json())
+        # print('response.status_code', response.status_code)
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json().get('success'), False)
+        self.assertEqual(response.json().get('success'), 'False')
         self.assertTrue('message' in response.json())
-
 
     def test_20_schema_info_parsing(self):
         """Retrieve the correct dataset from schema info, using File Ids"""
@@ -119,7 +145,6 @@ class FileViewGetTest(TestCase):
         self.assertTrue(file_resp.success is False)
         self.assertTrue(file_resp.message.find(bad_file_id) > -1)
 
-
         # Schema does NOT contain file info, bad id as int
         #
         bad_file_id = 99999
@@ -129,7 +154,6 @@ class FileViewGetTest(TestCase):
                                             file_persistent_id=schema_test_data.schema_info_01_file_pid)
         self.assertTrue(file_resp.success is False)
         self.assertTrue(file_resp.message.find(str(bad_file_id)) > -1)
-
 
     def test_40_schema_info_parsing(self):
         """Retrieve the correct dataset from schema info, uses DOIs"""
@@ -143,6 +167,6 @@ class FileViewGetTest(TestCase):
             file_persistent_id=schema_test_data.schema_info_02_file_pid)
 
         self.assertTrue(file_resp.success is True)
-        print(file_resp.data)
+        # print(file_resp.data)
         self.assertTrue('contentUrl' in file_resp.data)
         self.assertTrue(file_resp.data['identifier'].endswith(schema_test_data.schema_info_02_file_pid))
