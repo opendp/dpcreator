ef1f67a5881ea3642e36aff20bd1709d07b0b64e
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-05-13 12:27:17 -0400

    Added error handling for "file locked" status - added an alert to the Welcome page

--- client/cypress/integration/wecome-page-spec.js
+++ client/cypress/integration/wecome-page-spec.js
@@ -31,6 +31,20 @@
             cy.get('.v-btn--is-elevated > .v-btn__content').click({force: true});
             cy.get('.soft_primary.rounded-lg.mt-10.pa-16').should('contain',
                 ' doi:10.7910/DVN/PUXVDH | Replication Data for: Eye-typing experiment | Fatigue_data.tab ')
-        })
+        }),
+            it('Correctly displays locked file message', () => {
+                cy.visit('http://localhost:8000/mock-dv');
+                cy.get('#postOpenDP > .v-btn__content').click();
+                cy.url().should('contains', 'http://localhost:8000/');
+                cy.get('.v-input--selection-controls__ripple').click();
+                cy.get('#account-buttons--placeholder .v-btn--is-elevated > .v-btn__content').click();
+                cy.get('#input-65').click();
+                cy.get('#input-65').type('test_user');
+                cy.get('#input-68').click();
+                cy.get('#input-68').type('dpcreator');
+                cy.get('.v-btn--is-elevated').click();
+                cy.get('.mb-5 .v-alert__content').click();
+                cy.get('.v-alert__wrapper').should('contain', 'File is locked by another user')
+            })
     })
 }
