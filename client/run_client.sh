7a32139082a67aba708d1a488ecc1bf4ffdade69
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-05-17 17:29:44 -0400

    add conditional logic to run_client.sh

--- client/run_client.sh
+++ client/run_client.sh
@@ -2,4 +2,11 @@
 
 mkdir -p /code/client/src/locales &&
 node ./build_locale/CreateLocaleJson.js &&
-npm run serve --quiet
+echo "$DEV_MODE"
+if [ "$DEV_MODE" == "true" ]; then
+  echo 'dev mode'
+  npm run build
+else
+  echo 'not dev mode'
+  npm  run serve
+fi
