f2115d9cb9f742d66974faed17aaa283faa73334
Raman Prasad <raman_prasad@harvard.edu>
2021-05-17 15:54:10 -0400

    Update run_client.sh

--- client/run_client.sh
+++ client/run_client.sh
@@ -6,5 +6,5 @@ mkdir -p /code/client/src/locales
 printf "\n-- (20) Run CreateLocaleJson.js "
 node ./build_locale/CreateLocaleJson.js 
 
-printf "\n-- (30) 'npm run server --quiet' "
+# printf "\n-- (30) 'npm run server --quiet' "
 # npm run serve --quiet
