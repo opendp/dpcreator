45492506ccf5c5c88d99adacc0615712fbbcd645
ecowan <ethan24@gmail.com>
2021-05-19 13:20:08 -0400

    Allowing only certain fields to be updated via patch

---
+++ server/opendp_apps/dataset/tests/test_dataset_info.py
@@ -0,0 +1,56 @@
+import requests_mock
+
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from opendp_apps.dataverses.testing.test_endpoints import BaseEndpointTest
+from opendp_apps.model_helpers.msg_util import msgt
+
+
+@requests_mock.Mocker()
+class TestDataSetInfo(BaseEndpointTest):
+
+    fixtures = ['test_dataverses_01.json',
+                'test_manifest_params_04.json',
+                'test_opendp_users_01.json',
+                'test_dataset_data_001.json']
+
+    def setUp(self) -> None:
+        super(TestDataSetInfo, self).setUp()
+        self.user_obj, _created = get_user_model().objects.get_or_create(username='dv_depositor')
+        self.client.force_login(self.user_obj)
+
+    def test_successful_patch(self, req_mocker):
+        """(10) Successful patch"""
+        msgt(self.test_successful_patch.__doc__)
+        self.set_mock_requests(req_mocker)
+        response = self.client.patch(reverse("deposit-detail",
+                                             kwargs={'object_id': "9255c067-e435-43bd-8af1-33a6987ffc9b"}), {})
+        self.assertEqual(response.status_code, 200)
+        # print(response.json())
+        self.assertEqual(response.json(),
+                         {'object_id': '9255c067-e435-43bd-8af1-33a6987ffc9b', 'name': 'Fatigue_data.tab',
+                          'created': '2021-03-23T17:22:50.889000Z', 'creator': 'dev_admin',
+                          'installation_name': 'Harvard Dataverse', 'dataverse_file_id': 4164587,
+                          'dataset_doi': 'doi:10.7910/DVN/PUXVDH', 'file_doi': '', 'status': 'step_100',
+                          'depositor_setup_info': {'id': 1, 'created': '2021-03-23T17:22:50.889000Z',
+                                                   'updated': '2021-03-30T20:39:14.177000Z',
+                                                   'object_id': '9255c067-e435-43bd-8af1-33a6987ffc9b',
+                                                   'is_complete': False, 'user_step': 'step_100', 'epsilon': None,
+                                                   'dataset_questions': None, 'variable_ranges': None,
+                                                   'variable_categories': None}, 'dataset_schema_info': None,
+                          'file_schema_info': None})
+
+    def test_unsuccessful_patch(self, req_mocker):
+        msgt(self.test_unsuccessful_patch.__doc__)
+        self.set_mock_requests(req_mocker)
+        response = self.client.patch(reverse("deposit-detail",
+                                             kwargs={'object_id': "9255c067-e435-43bd-8af1-33a6987ffc9b"}),
+                                     {"dataset_schema_info": {"something": "this should fail"}}, format='json')
+        # print(f"patch response: {response.json()}")
+        # get_response = self.client.get(reverse("deposit-detail",
+        #                                        kwargs={'object_id': "9255c067-e435-43bd-8af1-33a6987ffc9b"}))
+        # print(f"get response: {get_response.json()}")
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message': 'These fields are not updatable',
+                                           'fields': ['dataset_schema_info']})
--- server/opendp_apps/dataset/views.py
+++ server/opendp_apps/dataset/views.py
@@ -1,4 +1,5 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, status
+from rest_framework.response import Response
 
 from opendp_apps.dataset.models import DataverseFileInfo, DataSetInfo
 from opendp_apps.dataset.serializers import DataverseFileInfoSerializer, \
@@ -18,7 +19,18 @@ class DataSetInfoViewSet(BaseModelViewSet):
         return self.queryset.filter(creator=self.request.user)
 
 
-class DepositorSetup(BaseModelViewSet):
+class DepositorSetupViewSet(BaseModelViewSet):
     queryset = DataverseFileInfo.objects.all()
     serializer_class = DataverseFileInfoSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def partial_update(self, request, *args, **kwargs):
+        acceptable_fields = ['user_step', 'dataset_questions', 'variable_ranges', 'variable_categories']
+        problem_fields = []
+        for field in request.data.keys():
+            if field not in acceptable_fields:
+                problem_fields.append(field)
+        if problem_fields:
+            return Response({'message': 'These fields are not updatable', 'fields': problem_fields},
+                            status=status.HTTP_400_BAD_REQUEST)
+        return super(DepositorSetupViewSet, self).partial_update(request, *args, **kwargs)
