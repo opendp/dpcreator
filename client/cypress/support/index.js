5b5b9f1728e71b3d4cdaabdbf652bbca7faf5def
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-06-29 18:28:57 -0400

    remove require terminal-report plugin

--- client/cypress/support/index.js
+++ client/cypress/support/index.js
@@ -15,7 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-require('cypress-terminal-report/src/installLogsCollector')();
+
+//require('cypress-terminal-report/src/installLogsCollector')();
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
