f0f14c52f3f476c9dd89202123de0fa39163b309
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-07-05 14:36:00 -0400

    add javascript library for converting object field names from snake case to camel case

--- client/src/api/dataset.js
+++ client/src/api/dataset.js
@@ -1,4 +1,5 @@
 import session from './session';
+const camelcaseKeys = require('camelcase-keys');
 
 export default {
     /**
@@ -6,6 +7,12 @@ export default {
      * @returns {Promise<AxiosResponse<any>>}
      */
     getUserDatasets() {
-        return session.get('/api/dataset-info/')
+        return session.get('/api/dataset-info/').then(resp => camelcaseKeys(resp, {deep: true}))
+
+    },
+    // Gets the DatasetInfo object for the given objectId
+    getDatasetInfo(objectId) {
+        return session.get('/api/dataset-info/' + objectId + '/').then(resp => camelcaseKeys(resp, {deep: true}))
+
     }
 }
