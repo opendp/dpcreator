d278809567356b2d6a97e5968273559821c3bdb8
ecowan <ethan24@gmail.com>
2021-07-20 13:50:23 -0400

    Fixing serializers for terms of access

--- server/opendp_apps/terms_of_access/serializers.py
+++ server/opendp_apps/terms_of_access/serializers.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
 
 from opendp_apps.terms_of_access.models import TermsOfAccess, TermsOfAccessLog
+from opendp_apps.user.models import OpenDPUser
 
 
-class TermsOfAccessSerializer(serializers.HyperlinkedModelSerializer):
+class TermsOfAccessSerializer(serializers.ModelSerializer):
     class Meta:
         model = TermsOfAccess
         fields = ['object_id', 'name', 'active', 'description', 'version', 'notes', 'created']
 
 
-class TermsOfAccessLogSerializer(serializers.HyperlinkedModelSerializer):
-    terms_of_access = TermsOfAccessSerializer(read_only=True)
+class TermsOfAccessLogSerializer(serializers.ModelSerializer):
+    terms_of_access = serializers.SlugRelatedField(queryset=TermsOfAccess.objects.all(),
+                                                   slug_field='object_id',
+                                                   read_only=False)
+    user = serializers.SlugRelatedField(queryset=OpenDPUser.objects.all(),
+                                        slug_field='object_id',
+                                        read_only=False)
 
     class Meta:
         model = TermsOfAccessLog
