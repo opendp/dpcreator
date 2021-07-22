6dce18b24892a78488d92eb8598d2fb5c65c2a7a
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-07-22 11:53:00 -0400

    add exception handler

--- client/cypress/integration/terms-and-conditions-spec.js
+++ client/cypress/integration/terms-and-conditions-spec.js
@@ -1,6 +1,12 @@
 {
     describe('Terms and Conditions test', () => {
         it('Seeds the database', () => {
+            cy.on('uncaught:exception', (e, runnable) => {
+                console.log('error', e)
+                console.log('runnable', runnable)
+
+                return false
+            })
             cy.clearData()
         })
     })
