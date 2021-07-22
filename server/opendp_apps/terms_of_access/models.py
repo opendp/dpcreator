7413c7f149dbfe555349282359fafcb30f7a2bea
ecowan <ethan24@gmail.com>
2021-07-22 14:10:16 -0400

    Undoing previous commit

--- server/opendp_apps/terms_of_access/models.py
+++ server/opendp_apps/terms_of_access/models.py
@@ -26,9 +26,8 @@ class TermsOfAccessLog(TimestampedModelWithUUID):
     """
     Records a Terms of Access agreement
     """
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, to_field="object_id", db_column="user", on_delete=models.PROTECT)
-    terms_of_access = models.ForeignKey(TermsOfAccess, to_field="object_id", db_column="terms_of_access",
-                                        on_delete=models.PROTECT)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    terms_of_access = models.ForeignKey(TermsOfAccess, on_delete=models.PROTECT)
 
     verbose_name = 'Terms of Access Log'
     verbose_name_plural = 'Terms of Access Logs'
