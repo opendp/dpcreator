fca649aab6f85dd642a0c9d0b2d479f56e1eee63
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-07-23 15:19:06 -0400

    add tests for Terms And Conditions page

--- client/cypress/integration/terms-and-conditions-spec.js
+++ client/cypress/integration/terms-and-conditions-spec.js
@@ -1,13 +1,21 @@
 {
     describe('Terms and Conditions test', () => {
-        it('Seeds the database', () => {
+        it('goes to Welcome page after accepting terms', () => {
             cy.on('uncaught:exception', (e, runnable) => {
                 console.log('error', e)
                 console.log('runnable', runnable)
-
                 return false
             })
             cy.clearData()
-        })
+            cy.login('dev_admin', 'admin')
+            cy.url().should('contain', 'terms-and-conditions')
+            cy.get('[data-test="confirmTermsCheckbox"]').click({force: true});
+            cy.get('[data-test="confirmTermsContinue"]').click();
+            cy.url().should('contain', 'welcome')
+        }),
+            it('goes directly to Welcome page when terms are already accepted', () => {
+                cy.login('dev_admin', 'admin')
+                cy.url().should('contain', 'welcome')
+            })
     })
 }
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -4,51 +4,14 @@ Cypress.Commands.add('login', (email, password) => {
     cy.get('[data-test="username"]').type(email);
     cy.get('[data-test="password"]').type(password);
     cy.get('[data-test="Log in"]').click();
-
-    // This test is necessary to prevent cypress from canceling the
-    // the POST to login to  the server (right now, login redirects to the Terms and Conditions Page)
-    cy.url().should('contain', 'terms-and-conditions')
+    // to force the login click, test that the browser went to the next  page
+    cy.url().should('not.contain', 'log-in')
 
 })
 Cypress.Commands.add('clearData', () => {
-    cy.visit('/log-in')
-    cy.get('[data-test="username"]').type('dev_admin');
-    cy.get('[data-test="password"]').type('admin');
-    cy.get('[data-test="Log in"]').click();
-
-    // This test is necessary to prevent cypress from canceling the
-    // the POST to login to  the server (right now, login redirects to the Terms and Conditions Page)
-    cy.url().should('contain', 'terms-and-conditions')
-    let token = cy.getCookie('csrftoken')
-    console.log('token:' + token)
-    //TODO: clear-test-data is not clearing OpenDPUsers, only DataverseUsers
-    // After clearing the data, repopulate it with seed data
-    // CSRF Failed: CSRF token missing or incorrect
-    /*var $crf_token = $('[name="csrfmiddlewaretoken"]').attr('value');
-the included it on jquery headers as follow
-
- $.ajax({
-            type: "POST",
-            url: "/api/endpoint/",
-            data: newEndpoint,
-            headers:{"X-CSRFToken": $crf_token},
-            success: function (newEnd) {
-                console.log(newEnd);
-                add_end(newEnd);
-            },
-            error: function () {
-                alert("There was an error")
-            }
-        });
-*/
+    cy.login('dev_admin', 'admin')
     cy.request('/cypress-tests/clear-test-data/')
-    //   .then(
-    //    () => {
-    //        cy.fixture("termsOfAccess.json").then((terms) => {
-    //            let headers = {"X-CSRFToken": token}
-    //           cy.request({method: 'POST', url: '/api/terms-of-access/', body: terms, headers: headers})
-    //      })
-    //  })
+
 
 })
 
