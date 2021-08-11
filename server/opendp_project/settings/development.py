40e12e52cd47ca23e84fbfaca7917fabe84a383e
Raman Prasad <raman_prasad@harvard.edu>
2021-08-11 10:59:11 -0400

    return to 20 variables in test #249

--- server/opendp_project/settings/development.py
+++ server/opendp_project/settings/development.py
@@ -5,6 +5,6 @@ from opendp_project.settings.base import *
 
 DEBUG = True
 
-PROFILER_DEFAULT_COLUMN_INDICES = json.loads(os.environ.get('PROFILER_DEFAULT_COLUMN_INDICES',
-                                                            '[0, 1, 2, 3, 4, 5, 6]'))
+#PROFILER_DEFAULT_COLUMN_INDICES = json.loads(os.environ.get('PROFILER_DEFAULT_COLUMN_INDICES',
+#                                                            '[0, 1, 2, 3, 4, 5, 6]'))
 
