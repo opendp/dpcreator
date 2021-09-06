d385402070e371d943bfd84d6dfdac7d728e2e6e
Raman Prasad <raman_prasad@harvard.edu>
2021-09-06 11:30:16 -0400

    nginx websocket url update for #282

--- server/opendp_project/asgi_azure.py
+++ server/opendp_project/asgi_azure.py
@@ -6,25 +6,24 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
-
 import os
 
 import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opendp_project.settings.azure_test_01')
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 from opendp_apps.async_messages import routing as async_routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'opendp_project.settings.azure_test_01')
-
-django.setup()
 
 application = ProtocolTypeRouter({
   "http": AsgiHandler(),
   "websocket": AuthMiddlewareStack(
     URLRouter(
-      async_routing.websocket_urlpatterns
+      async_routing.websocket_urlpatterns   # ws://async_messages/ws
     )
   ),
 })
--- server/opendp_project/settings/azure_test_01.py
+++ server/opendp_project/settings/azure_test_01.py
@@ -1,3 +1,4 @@
+import os
 from opendp_project.settings.base import *
 
 # ----------------------------------
