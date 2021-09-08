2fbcbed6b9248910553403456bf123261b2ad101
Raman Prasad <raman_prasad@harvard.edu>
2021-09-07 21:54:32 -0400

    added epsilon validator #234

--- server/opendp_apps/utils/extra_validators.py
+++ server/opendp_apps/utils/extra_validators.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 
 VALIDATE_MSG_ZERO_OR_GREATER = 'The value must be a number, zero or greater.'
+VALIDATE_MSG_EPSILON = 'As a rule of thumb, epsilon should not be less than 0.001 nor greater than 1.'
 
 def validate_not_negative(value):
     """Valid values are non-negative numbers"""
@@ -15,3 +16,12 @@ def validate_not_negative_or_none(value):
         return
 
     return validate_not_negative(value)
+
+def validate_epsilon_or_none(value):
+    """Rule of thumb from the non-technical primer: It is not recommended to have an epsilon > 1 or less than 0.001 (1/1000th)"""
+    if value is None:
+        # okay value!
+        return
+
+    if value > 1 or value < 0.001:
+        raise ValidationError(VALIDATE_MSG_EPSILON)
