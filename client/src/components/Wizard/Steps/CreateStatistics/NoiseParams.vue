0c70206cea267b41f4e30325a04e52836cac61b4
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2021-09-09 07:54:54 -0400

    276 epsilon delta ci (#286)
    * Save Epsilon questions, and set default epsilon and delta
    
    * add logic for assigning and redistributing delta values in statistics table
    
    * initializing Create Statistics Form from wizard, adding edit logic to Delta column in stats table
    
    * set Delta to NA when statistic isn't applicable
    
    * Save confidence Interval, move logic for saving epsilon & delta
    
    * include fix for #281
    
    * add validator to check for duplicate statistics in Add Statistic Dialog
    
    * hide error box when a statistic is valid #276
    
    Co-authored-by: Raman Prasad <raman_prasad@harvard.edu>

--- client/src/components/Wizard/Steps/CreateStatistics/NoiseParams.vue
+++ client/src/components/Wizard/Steps/CreateStatistics/NoiseParams.vue
@@ -24,7 +24,7 @@
           <span>Delta (x)</span>
           <span>
             {{ delta }}
-            <v-icon right @click="emitEditEvent">mdi-pencil</v-icon>
+            <v-icon right :disabled="delta === 0" @click="emitEditEvent">mdi-pencil</v-icon>
           </span>
         </div>
         <router-link to="/more-information#more-info-delta" class="primary--text caption">More information about Delta
@@ -37,7 +37,7 @@
         >
           <span>Confidence level</span>
           <span>
-            {{ confidenceLevel }}
+            {{ confidenceInterval }}
             <v-icon right @click="emitEditEvent">mdi-pencil</v-icon>
           </span>
         </div>
@@ -59,7 +59,7 @@
 <script>
 export default {
   name: "NoiseParams",
-  props: ["epsilon", "delta", "confidenceLevel"],
+  props: ["epsilon", "delta", "confidenceInterval"],
   methods: {
     emitEditEvent: function () {
       this.$emit("editNoiseParams");
