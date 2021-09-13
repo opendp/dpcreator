4cc82f06655e467842d8116d33f7788d7ba8513b
Ethan Cowan <ethan24@gmail.com>
2021-09-13 13:38:14 -0400

    275 release (#298)
    * DP Mean function
    
    * Sketching out ideas for release view
    
    * Reading from AnalysisPlan
    
    * Update to 0.2.0 library and related function signature changes #275
    
    * Fixing request handling in view
    
    * Reading min and max values from variable info, and adding validation
    
    * Adding column_index and statistic to result object
    
    * Working on method to run computation chains
    
    Co-authored-by: Raman Prasad <raman_prasad@harvard.edu>

---
+++ server/opendp_apps/dataset/tests/test_release_view.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import get_user_model
+from django.test.testcases import TestCase
+from rest_framework.test import APIClient
+
+from opendp_apps.analysis.analysis_plan_util import AnalysisPlanUtil
+from opendp_apps.analysis.models import AnalysisPlan
+from opendp_apps.dataset.models import DataSetInfo
+from opendp_apps.model_helpers.msg_util import msgt
+
+
+class TestReleaseView(TestCase):
+
+    fixtures = ['test_dataset_data_001.json', ]
+
+    def setUp(self):
+        self.client = APIClient()
+
+        self.user_obj, _created = get_user_model().objects.get_or_create(username='dev_admin')
+
+        self.client.force_login(self.user_obj)
+
+        self.request = {
+            "analysis_plan_id": 0,
+            "dp_statistics": [{
+                "error": "",
+                "label": "EyeHeight",
+                "locked": False,
+                "epsilon": 0.0625,
+                "variable": "eyeHeight",
+                "statistic": "mean",
+                "fixed_value": "5",
+                "handle_as_fixed": True,
+                "missing_values_handling": "insert_fixed"
+            }]
+        }
+
+    def test_post(self):
+        msgt(self.test_post.__doc__)
+
+        client = APIClient()
+        dataset_info = DataSetInfo.objects.get(id=4)
+        AnalysisPlanUtil.create_plan(dataset_info.object_id, self.user_obj)
+        analysis_plan = AnalysisPlan.objects.first()
+        self.request['analysis_plan_id'] = analysis_plan.object_id
+        response = client.post('/api/release/', data=self.request, format='json')
+        self.assertEqual(response.status_code, 201)
