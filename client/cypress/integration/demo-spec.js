fff5b95a18327303e5ca8e45f48a1ab5f7250f64
ellenk <ekraffmiller@hmdc.harvard.edu>
2021-09-16 17:32:51 -0400

    add exception handling, test Welcome page

--- client/cypress/integration/demo-spec.js
+++ client/cypress/integration/demo-spec.js
@@ -8,8 +8,16 @@
             })
         })
         it('uses the test Dataverse', () => {
+            cy.on('uncaught:exception', (e, runnable) => {
+                console.log('error', e)
+                console.log('runnable', runnable)
+                return false
+            })
             cy.clearData()
             cy.createMockDataset('dataverseMockDV.json')
+            cy.url().should('contain', 'welcome')
+            cy.get('.soft_primary.rounded-lg.mt-10.pa-16').should('contain',
+                ' Teacher Climate Survey ')
 
 
         })
