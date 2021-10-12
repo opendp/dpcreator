c4749faa0a940b9f27f02ac2ce4cb540cfc8f078
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2021-10-12 16:36:54 -0400

    351 wizard navigation (#371)
    * add test of display accuracy in statistics table
    
    * allow user to go back in Wizard without changing the current userStep
    
    * show correct dataset status and options in My Data table
    
    * Show time remaining in the My Data table, format analysisPlan updated time
    
    * display more dataset details on My Details page
    
    * show workflow and error statuses in My Details page
    
    * Add test for Wizard Navigation, re-organized shared test code
    
    * update django version
    
    Co-authored-by: Raman Prasad <raman_prasad@harvard.edu>

--- client/cypress/integration/my-data-spec.js
+++ client/cypress/integration/my-data-spec.js
@@ -13,6 +13,11 @@
 
             cy.get('tr').should('contain',
                 'Replication Data for: Eye-typing experiment')
+            cy.get('tr').should('contain',
+                'Uploaded')
+            cy.get('[data-test="continueWorkflow"]').click({force: true});
+            cy.get('h1').should('contain', 'Validate Dataset')
+
         })
     })
 }
