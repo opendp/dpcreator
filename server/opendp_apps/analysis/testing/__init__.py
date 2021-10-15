0ca50072cb0d855b584cd1e17c4724b046d1ce3b
Ethan Cowan <ethan24@gmail.com>
2021-10-15 17:42:24 -0400

    DP Sum spec class and refactoring tests (#376)
    * DP Sum spec class and refactoring tests
    
    * add DP sum and related test to ValidateReleaseUtil #321
    
    * updated test #321

--- server/opendp_apps/analysis/testing/__init__.py
+++ server/opendp_apps/analysis/testing/__init__.py
@@ -8,4 +8,12 @@ python manage.py test opendp_apps.analysis.testing.test_dp_count_spec.DPCountSta
 python manage.py test opendp_apps.analysis.testing.test_run_release.TestRunRelease.test_10_compute_stats
 
 python manage.py test opendp_apps.analysis.testing.test_dp_histogram_spec.HistogramStatSpecTest.test_105_run_dphist_calculation_categorical
+
+python manage.py test opendp_apps.analysis.testing.test_dp_sum_spec.DPSumStatSpecTest
+
+.test_105_run_dphist_calculation_categorical
+
+python manage.py test opendp_apps.analysis.testing.test_run_release.TestRunRelease.test_90_dp_count_pums_data
+
+
 """
