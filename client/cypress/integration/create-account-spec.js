c1700517d8d0386bf88a0bb75a052b412c3c5243
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2021-11-18 12:11:27 -0500

    96 my details (#398)
    * add watch function to update epsilon field; added tests
    
    * in the logoutHandler(), check current path before rerouting
    
    * refactoring, add comments, remove unneeded logging
    
    * change UI to text and variable names to confidence level, with options .99 and .95
    
    * implement save password action
    
    * move PUMS demo back into test folder
    
    * refactoring Cypress code - consistent test names
    
    * refactoring Cypress code - remove older files
    
    * refactoring Cypress code -add new files
    
    * use data-test property for clicking on login button
    
    * Add validation error message
    
    * change TermsOfAccessViewSet permissions to public
    
    * add test for Sign Up terms of use
    
    * move recording of acceptTerms from account creation to first time login
    
    * add removal of non-admin users in clear_test_data
    
    * turn off email verification for Cypress tests
    
    * add logic for accepting terms on first time login
    
    * add Cypress tests for account creation
    
    * fix terms and conditions logic after login
    
    * fix terms logic in tests
    
    * Add validation message when the username or email is already taken
    
    * MyProfile - allow username edit only
    
    * update terms.json
    
    * fixed formatting of error message
    
    Co-authored-by: Raman Prasad <raman_prasad@harvard.edu>

---
+++ client/cypress/integration/create-account-spec.js
@@ -0,0 +1,81 @@
+{
+    describe('Create Account Test', () => {
+        it('Creates a Dataverse user with Handoff', () => {
+            Cypress.Cookies.debug(true)
+            cy.on('uncaught:exception', (e, runnable) => {
+                console.log('error', e)
+                console.log('runnable', runnable)
+
+                return false
+            })
+            cy.clearData()
+            const username = 'kermit'
+            const email = 'kermit@thefrog.com'
+            const password = 'kermit123!'
+            cy.visit('/mock-dv');
+            cy.get('[data-test="submit button"]').click();
+            cy.url().should('contains', '/?id=');
+            cy.scrollTo("bottom");
+            cy.get('[data-test="termsOfServiceCheckbox"]').click({force: true});
+            cy.get('[data-test="createAccountButton"]').click({force: true, multiple: true});
+            cy.url().should('contain', 'sign-up')
+            cy.createAccount(username, email, password)
+            cy.url().should('contains', 'confirmation')
+            // We have turned off email confirmation for Cypress, so we should be able
+            // to log in now with the new account.
+            cy.visit('/log-in')
+            cy.get('[data-test="username"]').type(username);
+            cy.get('[data-test="password"]').type(password);
+            cy.get('[data-test="Log in"]').click();
+            cy.url().should('contains', 'welcome')
+            cy.get('[data-test="My Profile"]').click();
+            cy.url().should('contains', 'my-profile')
+            cy.get('[data-test="username"]').should('have.value', username)
+
+
+        })
+        it('Shows error message if user name is taken', () => {
+            cy.clearData()
+            cy.on('uncaught:exception', (e, runnable) => {
+                console.log('error', e)
+                console.log('runnable', runnable)
+
+                return false
+            })
+            const username = 'oscar'
+            const email = 'oscar@thegrouch.com'
+            const password = 'oscar123!'
+            cy.visit('/sign-up')
+            cy.createAccount(username, email, password).then(() => {
+                cy.url().should('contains', 'confirmation')
+                cy.visit('/sign-up')
+                cy.createAccount(username, email, password)
+                cy.get('[data-test="errorMessage"]').contains('A user with that username already exists.')
+            })
+
+
+        })
+        it('Creates an OpenDP user with no Handoff', () => {
+            cy.clearData()
+            cy.on('uncaught:exception', (e, runnable) => {
+                console.log('error', e)
+                console.log('runnable', runnable)
+
+                return false
+            })
+            const username = 'oscar'
+            const email = 'oscar@thegrouch.com'
+            const password = 'oscar123!'
+            cy.visit('/sign-up')
+            cy.createAccount(username, email, password)
+            cy.url().should('contains', 'confirmation')
+            cy.visit('/log-in')
+            cy.get('[data-test="username"]').type(username);
+            cy.get('[data-test="password"]').type(password);
+            cy.get('[data-test="Log in"]').click();
+            cy.get('[data-test="My Profile"]').click();
+            cy.get('[data-test="username"]').should('have.value', username)
+        })
+
+    })
+}
--- client/cypress/integration/terms-and-conditions-spec.js
+++ client/cypress/integration/terms-and-conditions-spec.js
@@ -1,18 +1,39 @@
 {
     describe('Terms and Conditions test', () => {
-        it('goes to My Data page after accepting terms', () => {
+        it('goes to Terms and Conditions if current terms not accepted', () => {
             cy.on('uncaught:exception', (e, runnable) => {
                 console.log('error', e)
                 console.log('runnable', runnable)
                 return false
             })
             cy.clearData()
-            cy.login('dev_admin', 'admin')
-            cy.url().should('contain', 'terms-and-conditions')
-            cy.get('[data-test="confirmTermsCheckbox"]').click({force: true});
-            cy.get('[data-test="confirmTermsContinue"]').click();
-            cy.url().should('contain', 'my-data')
-            cy.logout()
+            // create account
+            // login
+            // get user objectId
+            // logout
+            // intercept terms of access, to trigger needing to accept terms
+            // login again - should go to terms of access
+            const username = 'bigbird'
+            const email = 'bigbird@nest.com'
+            const password = 'bigbird123!'
+            cy.createAccount(username, email, password)
+            cy.login(username, password).then(() => {
+                console.log(sessionStorage)
+                const sessionObj = JSON.parse(sessionStorage.getItem('vuex'))
+                const userObjectId = sessionObj.auth.user.objectId
+                // simulate the user having excepted older terms of use
+                cy.intercept('GET', '/api/terms-of-access-agreement/', {
+                    body: {
+                        "count": 1, "next": null, "previous": null,
+                        "results": [{"user": userObjectId, "terms_of_access": "old-object-id"}]
+                    }
+                })
+                cy.logout()
+                cy.login(username, password)
+                cy.url().should('contains', 'terms')
+            })
+
+
         }),
             it('goes directly to My Data page when terms are already accepted', () => {
                 cy.on('uncaught:exception', (e, runnable) => {
@@ -20,8 +41,35 @@
                     console.log('runnable', runnable)
                     return false
                 })
-                cy.login('dev_admin', 'admin')
-                cy.url().should('contain', 'my-data')
+                cy.clearData()
+                // create account
+                // login
+                // get user objectId and current terms Id
+                // logout
+                // intercept terms of access, to simulate user already excepted terms
+                // login again - should go to my-data
+
+                const username = 'bigbird'
+                const email = 'bigbird@nest.com'
+                const password = 'bigbird123!'
+                cy.createAccount(username, email, password)
+
+                cy.login(username, password).then(() => {
+                    console.log(sessionStorage)
+                    const sessionObj = JSON.parse(sessionStorage.getItem('vuex'))
+                    const userObjectId = sessionObj.auth.user.objectId
+                    const currentTermsId = sessionObj.auth.currentTerms.objectId
+                    // simulate the user having already accepted current terms of use
+                    cy.intercept('GET', '/api/terms-of-access-agreement/', {
+                        body: {
+                            "count": 1, "next": null, "previous": null,
+                            "results": [{"user": userObjectId, "terms_of_access": currentTermsId}]
+                        }
+                    })
+                    cy.logout()
+                    cy.login(username, password)
+                    cy.url().should('contains', 'my-data')
+                })
 
             })
     })
