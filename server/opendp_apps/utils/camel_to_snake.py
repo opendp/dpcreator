1f8ed9f041007787f1734530b496736c91890ac2
Raman Prasad <raman_prasad@harvard.edu>
2021-12-07 13:58:03 -0500

    update to `camel_to_snake`, initial fix for #434  (need to add test)

--- server/opendp_apps/utils/camel_to_snake.py
+++ server/opendp_apps/utils/camel_to_snake.py
@@ -3,10 +3,17 @@ Utility function
 """
 import re
 
-def camel_to_snake(name):
+RE_REMOVE_SPACE = re.compile(r"\s+")
+RE_CAMEL_TO_SNAME = re.compile(r'(?<!^)(?=[A-Z])')
+
+def camel_to_snake(name: str) -> str:
     """
     Front end is passing camelCase, but JSON in DB is using snake_case
     :param name:
     :return:
     """
-    return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+    name = RE_REMOVE_SPACE.sub('', name)
+    #
+    return RE_CAMEL_TO_SNAME.sub('_', name).lower()
+    #
+    # return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
