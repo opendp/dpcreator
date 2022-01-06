11a4df8b601f3cf45212ec78c37b34f4e9a3a088
ellenk <ekraffmiller@hmdc.harvard.edu>
2022-01-06 08:16:22 -0500

    create a wrapper for axios calls that throws exception for unexpected errors

--- client/src/api/dataset.js
+++ client/src/api/dataset.js
@@ -1,4 +1,4 @@
-import session from './session';
+import {session, wrappedSession} from './session';
 const camelcaseKeys = require('camelcase-keys');
 
 export default {
@@ -7,16 +7,16 @@ export default {
      *
      */
     getUserDatasets() {
-        return session.get('/api/dataset-info/').then(resp => camelcaseKeys(resp, {deep: true}))
+        return wrappedSession.get('/api/dataset-info/').then(resp => camelcaseKeys(resp, {deep: true}))
 
     },
     // Gets the DatasetInfo object for the given objectId
     getDatasetInfo(objectId) {
-        return session.get('/api/dataset-info/' + objectId + '/').then(resp => camelcaseKeys(resp, {deep: true}))
+        return wrappedSession.get('/api/dataset-info/' + objectId + '/').then(resp => camelcaseKeys(resp, {deep: true}))
 
     },
     runProfiler(datasetId, userId) {
-        session.post('/api/profile/run-async-profile/',
+        wrappedSession.post('/api/profile/run-async-profile/',
             {object_id: datasetId})
             .then(resp => camelcaseKeys(resp, {deep: true}))
 
