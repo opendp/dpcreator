96e816f7edc982c4d1a7029a9435b5f20cebd755
Raman Prasad <raman_prasad@harvard.edu>
2022-01-18 12:34:58 -0500

    updated k8s spec #401

--- deploy/k8s_maker/dpcreator_specs_01.py
+++ deploy/k8s_maker/dpcreator_specs_01.py
@@ -15,7 +15,7 @@ specs_01 = dict(bird='song',
                 ALLOWED_HOSTS="40.85.170.176,dev.dpcreator.org,127.0.0.1,0.0.0.0",
                 loadBalancerIP="40.85.170.176",
                 #
-                ACCOUNT_EMAIL_VERIFICATION="none",  # "mandatory"
+                ACCOUNT_EMAIL_VERIFICATION="mandatory",  # "none"
                 )
 
 # test.dpcreator.org
@@ -32,6 +32,6 @@ specs_01_test = dict(bird='song',
                 ALLOWED_HOSTS="13.82.125.69,test.dpcreator.org,127.0.0.1,0.0.0.0",
                 loadBalancerIP="13.82.125.69",
                 #
-                ACCOUNT_EMAIL_VERIFICATION="none", # "mandatory"
+                ACCOUNT_EMAIL_VERIFICATION="mandatory",  # "none"
                 #
                 )
