d92d32577ac592b405248737a353958b20557bd2
ecowan <ethan24@gmail.com>
2022-02-25 15:58:43 -0500

    Including handoff_id in serializer

--- server/opendp_apps/user/serializers.py
+++ server/opendp_apps/user/serializers.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import Group
 from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from django.db import transaction
-from .models import OpenDPUser  # , Session
+from .models import OpenDPUser, DataverseUser
+from ..dataverses.models import DataverseHandoff
+from ..utils.view_helper import get_object_or_error_response
 
 """
 class SessionSerializer(serializers.HyperlinkedModelSerializer):
@@ -26,6 +28,7 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 
 class CustomRegisterSerializer(RegisterSerializer):
     objectId = serializers.models.UUIDField
+    handoffId = serializers.models.UUIDField
 
     # Define transaction.atomic to rollback the save operation in case of error
 
@@ -34,4 +37,7 @@ class CustomRegisterSerializer(RegisterSerializer):
         user = super().save(request)
         user.objectId = self.data.get('objectId')
         user.save()
+        handoff_obj = get_object_or_error_response(DataverseHandoff, object_id=self.data.get('handoffId'))
+        dataverse_user = DataverseUser(user=user, dv_installation=handoff_obj.dv_installation)
+        dataverse_user.save()
         return user
