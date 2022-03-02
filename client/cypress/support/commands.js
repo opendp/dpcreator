7e200b68c829ed91c9c32232a34543758c590385
ellenk <ekraffmiller@hmdc.harvard.edu>
2022-03-02 14:14:10 -0500

    updated fixtures for accuracy and rounded accuracy, moved fixedValue to stats fixture

--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -200,7 +200,7 @@ Cypress.Commands.add('createStatistics', (demoData) => {
         cy.get('[data-test="' + demoStat.statistic + '"]').click({force: true});
         const varDataTest = '[data-test="' + demoVar.name + '"]'
         cy.get(varDataTest).click({force: true})
-        cy.get('[data-test="Fixed value"]').type(demoVar.fixedValue)
+        cy.get('[data-test="Fixed value"]').type(demoStat.fixedValue)
         cy.get('[data-test="Create statistic"]').click({force: true})
 
         // The statistic should have been created
@@ -208,7 +208,7 @@ Cypress.Commands.add('createStatistics', (demoData) => {
         cy.get('tr').first().get('td').should('contain', demoStat.statistic)
         cy.get('table').contains('td', demoStat.statistic).should('be.visible');
         // Statistic should contain correct accuracy value
-        cy.get('table').contains('td', demoStat.accuracy).should('be.visible')
+        cy.get('table').contains('td', demoStat.roundedAccuracy).should('be.visible')
     })
 }),
 
