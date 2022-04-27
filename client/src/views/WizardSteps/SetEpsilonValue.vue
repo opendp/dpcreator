ad6f50fe3a5be40e3cfcaf2d0cf69d15cc487fda
ellenk <ekraffmiller@hmdc.harvard.edu>
2022-04-27 14:54:26 -0400

    remove up and down error from population size text field

--- client/src/views/WizardSteps/SetEpsilonValue.vue
+++ client/src/views/WizardSteps/SetEpsilonValue.vue
@@ -62,6 +62,13 @@
 </template>
 
 <style lang="scss">
+// Remove up an down error from text field when type == number
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
 .info-banner {
   .v-banner__content {
     align-items: unset;
