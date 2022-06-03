21be811a2435c857c7585eede3392bf74d2b66a8
ellenk <ekraffmiller@hmdc.harvard.edu>
2022-06-03 10:48:25 -0400

    updated Cypress package and add Cypress tests for direct file upload

---
+++ client/cypress/integration/file-upload-spec.js
@@ -0,0 +1,44 @@
+{
+    describe('File Upload Test', () => {
+        beforeEach(() => {
+            cy.on('uncaught:exception', (e, runnable) => {
+                console.log('error', e)
+                console.log('runnable', runnable)
+                return false
+            })
+            cy.clearData()
+            const username = 'kermit'
+            const email = 'kermit@thefrog.com'
+            const password = 'kermit123!'
+
+            cy.createAccount(username, email, password)
+
+        })
+        it('Uploads file with select input', () => {
+
+            cy.get('[data-test="My Data"]').click();
+            cy.url().should('contains', 'my-data')
+            cy.get('[data-test="myDataUploadButton"]').click();
+            cy.get('[data-test="fileInput"]').selectFile('cypress/fixtures/PUMS5extract1000.csv', {force: true})
+            cy.get('tr').should('contain',
+                'PUMS5extract1000.csv')
+            cy.get('tr').should('contain',
+                'Uploaded')
+
+        })
+        it('Uploads file with drag drop', () => {
+
+            cy.get('[data-test="My Data"]').click();
+            cy.url().should('contains', 'my-data')
+            cy.get('[data-test="myDataUploadButton"]').click();
+
+            cy.get('[data-test="dragArea"]').selectFile('cypress/fixtures/FultonPUMS5full.csv',
+                {force: true, action: 'drag-drop'})
+            cy.get('tr').should('contain',
+                'FultonPUMS5full.csv')
+            cy.get('tr').should('contain',
+                'Uploaded')
+
+        })
+    })
+}
