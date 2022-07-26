7a7e06487dd408f6979b39c29a47ac9df2560c33
ecowan <ethan24@gmail.com>
2022-07-26 12:41:58 -0400

    Reformatting opendp_project/

--- server/opendp_project/settings/development.py
+++ server/opendp_project/settings/development.py
@@ -1,6 +1,3 @@
-from opendp_project.settings.base import *
-
 DEBUG = True
 
 DEFAULT_LOGGER = 'console'
-
--- server/opendp_project/urls_cypress.py
+++ server/opendp_project/urls_cypress.py
@@ -4,8 +4,8 @@ Please read server/opendp_project/settings/cypress_settings for an explanation
 """
 from django.urls import path, include
 
-from opendp_project.urls import urlpatterns
 from opendp_apps.cypress_utils.check_setup import are_cypress_settings_in_place
+from opendp_project.urls import urlpatterns
 
 if are_cypress_settings_in_place:
     urlpatterns = [path('cypress-tests/', include('opendp_apps.cypress_utils.urls'))] \
