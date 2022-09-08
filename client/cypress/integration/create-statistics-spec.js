d083a89e585bd4218e5bc7cce6d6981a93f9c6a3
ellenk <ekraffmiller@hmdc.harvard.edu>
2022-09-08 14:17:27 -0400

    added Histogram option tests

--- client/cypress/integration/create-statistics-spec.js
+++ client/cypress/integration/create-statistics-spec.js
@@ -10,6 +10,42 @@
             cy.clearData()
 
         })
+        it('Saves Histogram Options Correctly', () => {
+            const mockDVfile = 'EyeDemoMockDV.json'
+            const demoDatafile = 'EyeDemoStatsTest.json'
+            cy.createMockDataset(mockDVfile)
+            cy.fixture(demoDatafile).then((demoData) => {
+                cy.url().should('contain', 'welcome')
+                cy.get('.soft_primary.rounded-lg.mt-10.pa-16').should('contain',
+                    demoData['datasetName'])
+                cy.goToConfirmVariables(demoData.variables)
+                // select the variables we will use
+                cy.selectVariable(demoData.variables)
+                // Continue to Set Epsilon Step
+                cy.epsilonStep()
+                //go to Create Statistics Step
+                cy.get('[data-test="wizardContinueButton"]').last().click({force: true});
+                cy.get('h1').should('contain', 'Create Statistics').should('be.visible')
+
+                // Add all a Histogram  statistic  and  test the options
+                cy.get('[data-test="Add Statistic"]').click({force: true});
+                cy.get('[data-test="AddStatisticDialog"]').should('be.visible')
+
+                cy.get('[data-test="Histogram"]').click({force: true});
+                const varDataTest = '[data-test="Trial"]'
+                cy.get(varDataTest).click({force: true})
+                cy.get('[data-test="Fixed value"]').type('5')
+                cy.get('[data-test="onePerValue"]').click({force: true})
+                cy.get('[data-test="Create Statistic Button"]').should('be.enabled')
+                cy.get('[data-test="equalRanges"]').click({force: true})
+                cy.get('[data-test="histogramNumberOfBins"]').type('3')
+                cy.get('div').should('contain', 'Equal range bins: [0, 4],[5, 10],uncategorized')
+                cy.get('[data-test="Create Statistic Button"]').click({force: true})
+                cy.get('tr').first().get('td').should('contain', "Histogram")
+
+            })
+        })
+
         it('Updated dpStatistics Correctly', () => {
             const mockDVfile = 'EyeDemoMockDV.json'
             const demoDatafile = 'EyeDemoStatsTest.json'
