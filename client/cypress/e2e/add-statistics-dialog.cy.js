36cf4db95f9b2dfb54f623e3a6df9a74f168fc3d
ellenk <ekraffmiller@hmdc.harvard.edu>
2023-01-24 14:41:53 -0500

    add test for Variable list and Create Statistics enabled

--- client/cypress/e2e/add-statistics-dialog.cy.js
+++ client/cypress/e2e/add-statistics-dialog.cy.js
@@ -22,31 +22,58 @@
 
 
         it('Enables Create Statistics Button', () => {
-            cy.pause()
             cy.get('h1').should('contain', 'Create Statistics')
             cy.get('[data-test="Add Statistic"]').click({force: true});
             cy.get('[data-test="AddStatisticDialog"]').should('be.visible')
-            // The fixed input field should be visible after switching from Count to Mean
+
+            // The Create Statistics Button should be enabled after entering histogram edges
             cy.get('[data-test="Histogram"]').click({force: true});
             cy.get('[data-test="Trial"]').click({force: true})
             cy.get('[data-test="Fixed value"]').type('22')
-            cy.pause()
             cy.get('[data-test="binEdges"]').click({force: true})
             cy.get('[data-test="histogramBinEdges"]').type("2,6,", {force: true})
+            cy.get('[data-test="Create Statistic Button"]').should('be.enabled')
             cy.get('[data-test="Create Statistic Button"]').click({force: true})
             cy.get('[data-test="Create Statistics Title').should('be.visible')
             cy.get('[data-test="Add Statistic"]').should('be.visible')
 
             cy.get('[data-test="Add Statistic"]').click({force: true});
             cy.get('[data-test="AddStatisticDialog"]').should('be.visible')
-            // The fixed input field should be visible after switching from Count to Mean
+
+            // The second time through, the Create Statistics button should still be enabled
             cy.get('[data-test="Histogram"]').click({force: true});
             cy.get('[data-test="Trial"]').click({force: true})
             cy.get('[data-test="Fixed value"]').type('33')
             cy.get('[data-test="binEdges"]').click({force: true})
-            cy.pause()
             cy.get('[data-test="histogramBinEdges"]').type("4,8,", {force: true})
+            cy.get('[data-test="Create Statistic Button"]').should('be.enabled')
             cy.get('[data-test="Create Statistic Button"]').click({force: true})
+
+
+        })
+        it('Displays correct Variable list', () => {
+            cy.get('h1').should('contain', 'Create Statistics')
+            cy.get('[data-test="Add Statistic"]').click({force: true});
+            cy.get('[data-test="AddStatisticDialog"]').should('be.visible')
+            cy.get('[data-test="Subject"]').should('be.enabled')
+            cy.get('[data-test="Language"]').should('be.enabled')
+
+            // After clicking Mean, the non-numeric variables should not be visible
+            cy.get('[data-test="Mean"]').click({force: true})
+            cy.get('[data-test="Subject"]').should('not.exist')
+            cy.get('[data-test="Language"]').should('not.exist')
+            cy.get('[data-test="Trial"]').click({force: true})
+
+            cy.get('[data-test="Fixed value"]').type('22')
+            cy.get('[data-test="Create Statistic Button"]').click({force: true})
+            cy.get('[data-test="Create Statistics Title').should('be.visible')
+            cy.get('[data-test="Add Statistic"]').should('be.visible')
+
+            cy.get('[data-test="Add Statistic"]').click({force: true});
+            cy.get('[data-test="AddStatisticDialog"]').should('be.visible')
+            // The non-numeric fields should be visible when re-opening the Dialog
+            cy.get('[data-test="Subject"]').should('be.enabled')
+            cy.get('[data-test="Language"]').should('be.enabled')
 
 
         })
