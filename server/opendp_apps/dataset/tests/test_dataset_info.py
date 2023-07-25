44705f42dd346086830983d242f67eef57f237ee
Raman Prasad <raman_prasad@harvard.edu>
2023-07-25 11:55:49 -0400

    remove tests repeated elsewhere #756

--- server/opendp_apps/dataset/tests/test_dataset_info.py
+++ server/opendp_apps/dataset/tests/test_dataset_info.py
@@ -1,3 +1,7 @@
+from http import HTTPStatus
+
+from django.urls import reverse
+
 from opendp_apps.dataset.models import DepositorSetupInfo
 from opendp_apps.dataset.tests.dataset_test_base import DatasetTestBase
 from opendp_apps.model_helpers.msg_util import msgt
@@ -9,9 +13,40 @@ class TestDepositorInfo(DatasetTestBase):
     def setUp(self) -> None:
         super().setUp()
 
-    def test_10_successful_patch(self):
-        """(10) Successful patch"""
-        msgt(self.test_10_successful_patch.__doc__)
+    def test_010_successful_get(self):
+        """(10) Get DatasetInfo list with no objects"""
+        msgt(self.test_010_successful_get.__doc__)
+
+        response = self.client.get(reverse("datasetinfo-list"))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.json(), {'count': 0, 'next': None, 'previous': None, 'results': []})
+
+    def test_015_unsuccessful_get_not_logged_in(self):
+        """(15) Fail to get DatasetInfo b/c not logged in"""
+        msgt(self.test_015_unsuccessful_get_not_logged_in.__doc__)
+
+        self.client.logout()
+        response = self.client.get(reverse("datasetinfo-list"))
+        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
+        self.assertEqual(response.json(), {'detail': 'Authentication credentials were not provided.'})
+
+    def test_020_unallowed_post_bad_url(self):
+        """(20) POST should be blocked on '/api/dataset-info/', creates are specific to the DatasetInfo subclasses, such as UploadFileInfo"""
+        msgt(self.test_020_unallowed_post_bad_url.__doc__)
+
+        payload = dict(name=self.upload_name,
+                       creator=self.user_obj.object_id,
+                       source_file=self.test_file_obj)
+
+        print('reverse("datasetinfo-list")', reverse("datasetinfo-list"))
+        resp = self.client.post(reverse("datasetinfo-list"),
+                                data=payload)
+
+        self.assertEqual(resp.status_code, HTTPStatus.METHOD_NOT_ALLOWED)
+
+    def test_030_successful_patch(self):
+        """(30) Successful patch"""
+        msgt(self.test_030_successful_patch.__doc__)
 
         ds_dict = self.upload_file_via_api()
 
@@ -21,15 +56,15 @@ class TestDepositorInfo(DatasetTestBase):
         response = self.client.patch(self.API_DATASET_INFO + ds_dict['object_id'] + '/',
                                      data=update_payload,
                                      content_type="application/json")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.json()['object_id'], ds_dict['object_id'])
         self.assertEqual(response.json()['name'], update_payload['name'])
         self.assertEqual(response.json()['description'], update_payload['description'])
         self.assertEqual(response.json()['status'], DepositorSetupInfo.DepositorSteps.STEP_0100_UPLOADED)
 
-    def test_20_patch_restricted_field(self):
-        """(20) Try to patch a field that isn't allowed"""
-        msgt(self.test_20_patch_restricted_field.__doc__)
+    def test_040_patch_restricted_field(self):
+        """(40) Try to patch a field that isn't allowed"""
+        msgt(self.test_040_patch_restricted_field.__doc__)
 
         ds_dict = self.upload_file_via_api()
 
@@ -43,7 +78,7 @@ class TestDepositorInfo(DatasetTestBase):
                                      content_type="application/json")
 
         # should skip the restricted fields such as depositor_setup_info, source, and source_file
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
         # Match the original, no update
         self.assertEqual(response.json()['object_id'], ds_dict['object_id'])
@@ -53,9 +88,9 @@ class TestDepositorInfo(DatasetTestBase):
         # description is updated
         self.assertEqual(response.json()['description'], update_payload['description'])
 
-    def test_30_get_dataset_info(self):
-        """(30) get dataset info"""
-        msgt(self.test_30_get_dataset_info.__doc__)
+    def test_050_get_dataset_info(self):
+        """(50) get dataset info"""
+        msgt(self.test_050_get_dataset_info.__doc__)
 
         ds_dict = self.upload_file_via_api()
 
@@ -63,13 +98,13 @@ class TestDepositorInfo(DatasetTestBase):
 
         # should skip the restricted fields such as depositor_setup_info, source, and source_file
         print('response', response.json())
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.json()['object_id'], ds_dict['object_id'])
         self.assertEqual(response.json()['creator_id'], str(self.user_obj.object_id))
 
-    def test_40_get_dataset_info_list(self):
-        """(40) get dataset info list"""
-        msgt(self.test_40_get_dataset_info_list.__doc__)
+    def test_060_get_dataset_info_list(self):
+        """(60) get dataset info list"""
+        msgt(self.test_060_get_dataset_info_list.__doc__)
 
         ds_dict = self.upload_file_via_api()
         ds_dict2 = self.upload_second_file_via_api()
@@ -77,7 +112,7 @@ class TestDepositorInfo(DatasetTestBase):
 
         response = self.client.get(self.API_DATASET_INFO)
 
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(response.json()['count'], 2)
         self.assertTrue(response.json()['results'][0]['object_id'] in ds_info_object_ids)
         self.assertTrue(response.json()['results'][1]['object_id'] in ds_info_object_ids)
