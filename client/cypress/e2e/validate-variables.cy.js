c3cf4e84131ebe9177afc2fd91d24e03ecfa8f80
Ellen Kraffmiller <ekraffmiller@hmdc.harvard.edu>
2023-08-15 16:37:34 -0400

    fix validate-variables test

--- client/cypress/e2e/validate-variables.cy.js
+++ client/cypress/e2e/validate-variables.cy.js
@@ -1,21 +1,13 @@
 {
     describe('Confirm Variables Validation tests', () => {
-        before(() => {
-            cy.on('uncaught:exception', (e, runnable) => {
-                console.log('error', e)
-                console.log('runnable', runnable)
-                return false
-            })
-            cy.clearData()
-            //
-        })
+
         beforeEach(() => {
             cy.on('uncaught:exception', (e, runnable) => {
                 console.log('error', e)
                 console.log('runnable', runnable)
                 return false
             })
-
+            cy.clearData()
             cy.setupConfirmVariablesPage('datasetInfoStep400.json')
 
         })
